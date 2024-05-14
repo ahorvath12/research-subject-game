05b409dc7024a32b870abb1e3b3707f9fda2a46d
Andrea Horvath <andreahorvath1998@gmail.com>
2024-05-13 18:37:36 -0700

    fix target frame rate being too high

--- Research Subject/Assets/Scripts/GUI/SettingsManager.cs
+++ Research Subject/Assets/Scripts/GUI/SettingsManager.cs
@@ -29,7 +29,7 @@ public class SettingsManager : MonoBehaviour
 
         if (volumeSlider)
         {
-            volumeSlider.value = PlayerPrefs.GetFloat("volume", 0);
+            volumeSlider.value = PlayerPrefs.GetFloat("volume", volumeSlider.maxValue);
         }
 
     }
